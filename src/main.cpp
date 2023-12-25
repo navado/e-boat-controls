cbf7e31f9aa590afbccd48e663bb0000b92d4ea0
Boris Sorochkin <boris@nestlogic.com>
2023-12-25 23:25:00 +0300

    End of day state

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include <Wire.h>
 #endif
 
+/* ------------ PINOUT ---------------*/
+#pragma region PINOUT
 
 #define CS_PIN 8
 #define RSE_PIN 7
@@ -18,8 +20,23 @@
 #define SDI_PIN 10
 #define BACKLIGHT_PIN 9
 #define LED_BRD_RD 2
-#define LED_BRD_GR 3
+#define LED_BRD_GR 10
 
+#define RPM_PIN 3
+
+#define THROTTLE_IN A0
+#define THROTTLE_OUT 10
+
+#define BTN_D A2
+#define BTN_C A3
+#define BTN_B A4
+#define BTN_A A5
+#define BTN_PRESS_THRESHOLD 4
+#define BTN_LONG_PRESS_THRESHOLD 60
+#pragma endregion
+/* ------------ LCD ------------------*/
+
+#pragma region LCD
 #define CONTRAST_SETTING 0x3A
 
 U8G2_ST7565_ERC12864_ALT_F_4W_SW_SPI lcd(U8G2_R0,
@@ -29,6 +46,55 @@ U8G2_ST7565_ERC12864_ALT_F_4W_SW_SPI lcd(U8G2_R0,
                                         /* dc=*/RS_PIN,
                                         /* reset=*/RSE_PIN
                                         );
+
+#pragma endregion
+
+/* ------------ Variables ------------------*/
+#pragma region Variables
+volatile uint8_t frames = 0;
+volatile uint8_t fps = 0;
+volatile uint16_t cnt, cnt_raw = 0;
+
+
+#define THROTTLE_TABLE_SIZE 12
+const uint8_t throttle_table[] = {0, 66, 86, 106, 125, 145, 165, 185, 205, 224, 244, 255};
+
+volatile uint8_t throttle_index = 0;
+volatile uint8_t throttle_out_value = 0;
+#define THROTTLE_IN_STEP (499/1024.0)
+volatile uint16_t throttle_in_value = 0;
+
+typedef struct
+{
+  uint8_t state : 1; // 0 - released, 1 - pressed
+  uint8_t count: 7;
+} btn_state_t;
+
+btn_state_t buttons_state[4] = {0};
+btn_state_t * btn_a = &buttons_state[0];
+btn_state_t * btn_b = &buttons_state[1];
+btn_state_t * btn_c = &buttons_state[2];
+btn_state_t * btn_d = &buttons_state[3];
+
+#pragma endregion
+
+/* ------------ Functions ------------------*/
+
+void update_button_state(uint8_t index, uint8_t value){
+  btn_state_t * btn = &buttons_state[index];
+  btn->state = value;
+  btn->count = (btn->state == 1 && btn->count<120)? btn->count + 1 : btn->count;
+  if (btn->state == 0){
+    btn->count = 0;
+  }
+}
+
+void update_buttons(){
+  update_button_state(0, digitalRead(BTN_A));
+  update_button_state(1, digitalRead(BTN_B));
+  update_button_state(2, digitalRead(BTN_C));
+  update_button_state(3, digitalRead(BTN_D));
+}
 
 void setup() {
   Serial.begin(115200);
@@ -46,10 +112,41 @@ void setup() {
 
   lcd.setContrast(CONTRAST_SETTING);
   Serial.println("LCD Init Done");
+
+  // Prepare timings
+  TimeInterrupt.begin(PRECISION);
+  TimeInterrupt.addInterrupt([]()
+                             {
+                              fps = frames;
+                              frames = 0;
+                              cnt = cnt_raw;
+                              cnt_raw = 0; },
+                             1000); // 1s
+  TimeInterrupt.addInterrupt([]()
+                             { throttle_in_value = analogRead(THROTTLE_IN); 
+                             update_buttons();
+                             },
+                             100); // 100ms
+
+  pinMode(RPM_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(RPM_PIN), []() {
+    cnt_raw++;
+  }, FALLING);
+
+  // Throttle
+  pinMode(THROTTLE_IN, INPUT);
+  pinMode(THROTTLE_OUT, OUTPUT);
+  analogReference(DEFAULT);
+
+  // Buttons
+  pinMode(BTN_A, INPUT);
+  pinMode(BTN_B, INPUT);
+  pinMode(BTN_C, INPUT);
+  pinMode(BTN_D, INPUT);
+
 }
 
-char buf[16]={0};
-
+#pragma region UI
 void printSmiley(uint8_t x, uint8_t y, uint8_t r){
   uint8_t _eye_level = y - r/4;
   uint8_t _eye_size = r/8;
@@ -69,22 +166,55 @@ uint8_t printKWLabel(uint8_t x, uint8_t y, const char* key,  uint32_t value){
   return y+8;
 }
 
-uint8_t frames = 0, fps = 0;
-uint8_t current_pin = 0;
-uint16_t cnt = 0;
+uint8_t printKWLabel(uint8_t x, uint8_t y, const char* key, const char * value){
+  lcd.setFont(u8g2_font_5x8_tf);
+  lcd.setCursor(x, y);
+  lcd.print(key);
+  lcd.print(value);
+  return y+8;
+}
 
 void printColumn1(){
   uint8_t _x=2, _y = 8;
-  _y = printKWLabel(_x, _y, "RPM: ", cnt * 60);
+  _y = printKWLabel(_x, _y, "CNT: ", cnt);
+  _y = printKWLabel(_x, _y, "RPM: ", cnt * 10);
   _y = printKWLabel(_x, _y, "T: ", millis()/1000);
   _y = printKWLabel(_x, _y, "FPS: ", fps);
 }
 
+const char * btn_to_str_value(uint8_t btn){
+  switch (btn)
+  {
+  case 0:
+    return "OFF";
+  case 1:
+    return "ON";
+  default:
+    return "ERR";
+  }
+}
+
+uint8_t print_btn_state(uint8_t _x, uint8_t _y, uint8_t index){
+  btn_state_t * btn = &buttons_state[index];
+  lcd.setFont(u8g2_font_5x8_tf);
+  lcd.setCursor(_x, _y);
+  lcd.print("B-");
+  lcd.print((char)(index + 'A'));
+  lcd.print(": ");
+  lcd.print(btn_to_str_value(btn->state));
+  lcd.print(" ");
+  lcd.print(btn->count);
+  return _y+8;
+}
+
 void printColumn2(){
   uint8_t _x=64, _y = 8;
-  _y = printKWLabel(_x, _y, "CNT: ", cnt);
-  // _y = printKWLabel(_x, _y, "F_M: ", FrequencyMeasured);
-  // _y = printKWLabel(_x, _y, "D_M: ", DutycycleMeasured);
+  _y = printKWLabel(_x, _y, "T/I: ", throttle_index);
+  _y = printKWLabel(_x, _y, "T/O: ", throttle_out_value);
+  _y = printKWLabel(_x, _y, "T/V: ", throttle_in_value * THROTTLE_IN_STEP);
+  for (uint8_t i = 0;i<4;i++){
+    _y = print_btn_state(_x,_y, i);
+  }
 }
 
 void printSmileyFont(uint8_t index){
@@ -93,11 +223,7 @@ void printSmileyFont(uint8_t index){
   lcd.print(index);
 }
 
-void loop()
-{
-  digitalWrite(LED_BRD_GR, !digitalRead(LED_BRD_GR));
-  digitalWrite(LED_BRD_RD, !digitalRead(LED_BRD_RD));
-
+void draw_screen(){
   frames++;
   lcd.firstPage();
   do
@@ -109,4 +235,30 @@ void loop()
     printSmileyFont(frames%10);
 
   } while (lcd.nextPage());
+}
+#pragma endregion
+
+void handle_throttle(){
+  analogWrite(THROTTLE_OUT, throttle_out_value);
+  if (btn_a->state == 1 && btn_b->state == 0 && btn_a->count == BTN_PRESS_THRESHOLD){
+    btn_a->count = btn_a->count + 1;
+    if(throttle_index != THROTTLE_TABLE_SIZE - 1){
+      throttle_index = (throttle_index + 1) % THROTTLE_TABLE_SIZE;
+    }
+  }
+  if (btn_b->state == 1 && btn_a->state == 0 && btn_b->count == BTN_PRESS_THRESHOLD){
+    btn_b->count = btn_b->count + 1;
+    if (throttle_index != 0){
+      throttle_index = (throttle_index - 1) % THROTTLE_TABLE_SIZE;
+    }
+  }
+  throttle_out_value = throttle_table[throttle_index];
+}
+
+void loop()
+{
+  // digitalWrite(LED_BRD_GR, !digitalRead(LED_BRD_GR));
+  digitalWrite(LED_BRD_RD, !digitalRead(LED_BRD_RD));
+  handle_throttle();
+  draw_screen();
 }
